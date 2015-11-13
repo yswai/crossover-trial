dcad47074f60f65a33b1717004a8b1d1a2fdb04f
yswai <yswai1986@gmail.com>
2015-11-13 19:54:01 +0800

    Init

--- client/wwwroot/js/app.js
+++ client/wwwroot/js/app.js
@@ -1,6 +1,7 @@
 (function() {
 
    'use strict';
+
     var app = angular.module('app', ['ui.router']);
 
     app.config([function() {
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,40 @@
 var gulp = require('gulp');
 var del = require('del');
+var CacheBuster = require('gulp-cachebust');
+var cachebust = new CacheBuster();
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+
+var tmpFolder = 'client/tmp';
 var srcFolder = 'client/src';
 var distFolder = 'client/wwwroot';
 
-gulp.task('clean', function() {
-   return del([distFolder + '/css', distFolder + '/js', distFolder + '/img', ]);
+gulp.task('clean-tmp', function() {
+   return del(
+     [tmpFolder + '/styles', tmpFolder + '/js', tmpFolder + '/img']
+   );
 });
 
-gulp.task('dev', ['clean'], function() {
-  gulp.src([srcFolder + '/**'])
+gulp.task('clean-dist', function() {
+  return del(
+    [distFolder + '/styles', distFolder + '/js', distFolder + '/img']
+  );
+});
+
+gulp.task('build-css', function() {
+  return gulp.src(srcFolder + '/styles/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest(tmpFolder + '/styles'));
+});
+
+gulp.task('build-js', function() {
+  gulp.src(srcFolder + '/js/**.*')
+    .pipe(gulp.dest(tmpFolder + '/js'));
+});
+
+gulp.task('dev', ['clean-dist', 'clean-tmp', 'build-js', 'build-css'], function() {
+  gulp.src([tmpFolder + '/**'])
     .pipe(gulp.dest(distFolder));
 });
