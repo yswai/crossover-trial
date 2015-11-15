6de90480e81c70ceba27d7732052a4ce709308fa
yswai <yswai1986@gmail.com>
2015-11-15 13:15:11 +0800

    added highcharts

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -2,7 +2,7 @@
 
    'use strict';
 
-    var app = angular.module('app', ['ui.router', 'lbServices', 'slugifier']);
+    var app = angular.module('app', ['ui.router', 'lbServices', 'highcharts-ng', 'slugifier']);
 
     app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
